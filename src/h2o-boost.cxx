e14224295f5d3c158db4c075870dc208f1c6c87f
Michał Górny <mgorny@gentoo.org>
2012-03-28 14:59:54 +0200

    Support getting p & T.

--- src/h2o-boost.cxx
+++ src/h2o-boost.cxx
@@ -15,3 +15,17 @@ H2O::H2O(units::MPa p, units::K T)
 	: ::h2o::H2O(p.value(), T.value())
 {
 }
+
+units::MPa H2O::p() const
+{
+	units::MPa p(::h2o::H2O::p() * 1E6 * ::boost::units::si::pascals);
+
+	return p;
+}
+
+units::K H2O::T() const
+{
+	units::K T(::h2o::H2O::T() * ::boost::units::si::kelvins);
+
+	return T;
+}
